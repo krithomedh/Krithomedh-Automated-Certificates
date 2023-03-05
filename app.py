5710f34c479c2b122427b8afb695132590d930b5
ritish <sritish.10@gmail.com>
2023-03-05 11:07:56 +0530

    only csv access

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ global uploaded_file
 with st.sidebar:
     try:
         # Uploading file with except 
-        uploaded_file = st.file_uploader("Choose a file")
+        uploaded_file = st.file_uploader("Choose a file",type=['csv'])
 
         if uploaded_file is not None:
             data = pd.read_csv(uploaded_file)
